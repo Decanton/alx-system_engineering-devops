a51d6746642d4d2a1c41a9ceb00fc675fc6e80c5
Dickens Owuor <owuordickens0001@gmail.com>
2023-08-01 13:14:55 -0700

    tasks

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if there is exactly one argument
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <input>"
-  exit 1
-end
-
-# Extract the input from the command-line argument
-input = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /hb+t/
-
-# Use the scan method to find all occurrences of the pattern in the input
-matches = input.scan(pattern)
-
-# Output the matches
-puts matches.join("\n")
-
+# A regular expression that is matches a given pattern
+puts ARGV[0].scan(/hbt{2,5}n/).join
